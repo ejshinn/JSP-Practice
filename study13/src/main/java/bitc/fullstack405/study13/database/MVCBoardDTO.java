ec433855c29c4e4973e5adf3e40a38edaaaa5307
ejshinn <ejshin.dev@gmail.com>
2024-07-14 12:02:48 +0900

    MVC2 방식 게시판 : DTO 클래스 생성

---
+++ study13/src/main/java/bitc/fullstack405/study13/database/MVCBoardDTO.java
@@ -0,0 +1,106 @@
+package bitc.fullstack405.study13.database;
+
+// DB의 mvcboard 테이블과 데이터를 주고받기 위한 DTO 클래스
+public class MVCBoardDTO {
+    // 멤버 변수 선언
+    private int idx;
+    private String name;
+    private String title;
+    private String content;
+    private String postDate;
+    private String modifyDate;
+    private String oFile;
+    private String sFile;
+    private int downCount;
+    private String pass;
+    private int visitCount;
+
+    // getter/setter
+    public int getIdx() {
+    return idx;
+  }
+
+    public void setIdx(int idx) {
+    this.idx = idx;
+  }
+
+    public String getName() {
+    return name;
+  }
+
+    public void setName(String name) {
+    this.name = name;
+  }
+
+    public String getTitle() {
+    return title;
+  }
+
+    public void setTitle(String title) {
+    this.title = title;
+  }
+
+    public String getContent() {
+    return content;
+  }
+
+    public void setContent(String content) {
+    this.content = content;
+  }
+
+    public String getPostDate() {
+    return postDate;
+  }
+
+    public void setPostDate(String postDate) {
+    this.postDate = postDate;
+  }
+
+    public String getModifyDate() {
+    return modifyDate;
+  }
+
+    public void setModifyDate(String modifyDate) {
+    this.modifyDate = modifyDate;
+  }
+
+    public String getoFile() {
+    return oFile;
+  }
+
+    public void setoFile(String oFile) {
+    this.oFile = oFile;
+  }
+
+    public String getsFile() {
+    return sFile;
+  }
+
+    public void setsFile(String sFile) {
+    this.sFile = sFile;
+  }
+
+    public int getDownCount() {
+    return downCount;
+  }
+
+    public void setDownCount(int downCount) {
+    this.downCount = downCount;
+  }
+
+    public String getPass() {
+    return pass;
+  }
+
+    public void setPass(String pass) {
+    this.pass = pass;
+  }
+
+    public int getVisitCount() {
+    return visitCount;
+  }
+
+    public void setVisitCount(int visitCount) {
+    this.visitCount = visitCount;
+  }
+}
