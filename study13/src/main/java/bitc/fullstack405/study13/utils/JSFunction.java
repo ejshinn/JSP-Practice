607801859f1c2d4d23f5792cc5dc8cc6e2972561
ejshinn <ejshin.dev@gmail.com>
2024-07-14 12:03:25 +0900

    MVC2 방식 게시판 : 자바스크립트 사용 클래스 생성

---
+++ study13/src/main/java/bitc/fullstack405/study13/utils/JSFunction.java
@@ -0,0 +1,43 @@
+package bitc.fullstack405.study13.utils;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+
+public class JSFunction {
+
+    public static void alertLocation(String msg, String url, HttpServletResponse resp) {
+        try {
+            // 기존에는 jsp에서 제공하는 out 내장 객체를 매개변수로 받아서 사용
+            // 서블릿 파일은 확장자가 java이고 java는 out 내장 객체가 없음
+            // jsp에서 제공하는 out 내장 객체는 JspWriter 클래스를 사용하고 있으며, JspWriter는 PrintWriter를 상속받아 구현한 클래스임
+            // PrintWriter 객체는 HttpServletResponse 객체를 통해서 받아올 수 있음
+            resp.setContentType("text/html; charset=utf-8");
+            PrintWriter out = resp.getWriter();
+
+            String script = "<script>";
+            script += "alert('" + msg + "'); ";
+            script += "location.href = '" + url + "'; ";
+            script += "</script>";
+
+            out.println(script);
+    }
+    catch (Exception e) { }
+}
+
+    public static void alertBack(String msg, HttpServletResponse resp) {
+            try {
+                resp.setContentType("text/html; charset=utf-8");
+                PrintWriter out = resp.getWriter();
+
+        String script = "<script>";
+        script += "alert('" + msg + "'); ";
+        script += "history.back(); ";
+        script += "</script>";
+        out.println(script);
+    }
+    catch (Exception e) {
+        System.out.println("오류 발생 - " + e.getMessage());
+    }
+    }
+}
